c55a3c6e01a3767a63d2b6ad52544b510a584604
Dijana Pandurevic <dijana.pandurevic@gmail.com>
2020-03-05 21:16:38 +0100

    dodano lokalno - interenet

--- konfiguracija.php
+++ konfiguracija.php
@@ -1,13 +1,33 @@
 <?php
 
-return [
-    'nazivAPP' => 'Moje najbolje kiflice',
-    'url' => 'http://mojekiflice.hr/',
-    'dev' => true,
-    'db' =>[
-         'server' => 'localhost',
-         'baza' => 'zavrsni_rad',
-         'korisnik' => 'dijana',
-         'lozinka' => '0505'
-     ]
-]; 
+if(
+    $_SERVER['HTTP_HOST'] === 'localhost' ||
+    $_SERVER['SERVER_ADDR']==='127.0.0.1' ||
+    $_SERVER['HTTP_HOST'] === 'mojekiflice.hr'
+    ){
+
+    return [
+        'nazivAPP' => 'Moje kiflice LOKALNO',
+        'url' => 'http://mojekiflice.hr/',
+        'dev' => true,
+        'db' =>[
+            'server' => 'localhost',
+            'baza' => 'zavrsni_rad',
+            'korisnik' => 'dijana',
+            'lozinka' => '0505'
+            ]
+        ];
+    }else{
+
+        return [
+            'nazivAPP' => 'Moje kiflice INTERNET',
+            'url' => 'http://polaznik08.edunova.hr/',
+            'dev' => false,
+            'db' =>[
+                'server' => 'localhost',
+                'baza' => 'zavrsni_rad',
+                'korisnik' => 'dijana',
+                'lozinka' => '0505'
+            ]
+        ];
+        }
